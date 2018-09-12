965c7f53e463ee1131d11bcc37ee2aae98fb19b9
风 <1109695647@qq.com>
2018-09-12 16:12:04 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ src/components/icons/FontAwesome/Index.tsx
@@ -0,0 +1,8 @@
+import FontAwesome from 'react-native-vector-icons/glyphmaps/FontAwesome.json';
+import "./style.less";
+import createIconSet from "../CreateIconSet";
+
+const Icon = createIconSet(FontAwesome, 'FontAwesome');
+
+
+export default Icon;
